05364681c855442734f3cb421e68f7049505ef29
shubh34 <shubhamjain.qwerty@gmail.com>
2019-08-17 10:10:28 +0100

    Redux setup (#7)
    * Redux and folder structre
    
    * Redux component set up

--- src/components/ViewBlog/ViewBlog.tsx
+++ src/components/ViewBlog/ViewBlog.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
 import store from '../../store';
-import BlogsListing from '../BlogsListing/BlogsListing';
 
 const App: React.FC = () => {
   return (
     <Provider store={store()}>
-    <div className="App">
-      <BlogsListing name="Shubham"/>
-    </div>
+      <div className="App">
+        
+      </div>
     </Provider>
   );
 }
