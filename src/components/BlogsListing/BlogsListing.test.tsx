ae0203bdbcd356fdbc8e3bdd79fe2a64be8b1ff2
shubh34 <shubhamjain.qwerty@gmail.com>
2019-08-18 10:04:06 +0100

    formating (#20)

--- src/components/BlogsListing/BlogsListing.test.tsx
+++ src/components/BlogsListing/BlogsListing.test.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
 import { BlogsListing } from './BlogsListing';
-import {blogListsForListing} from '../../../tests_helpers/blogs';
-const setUp = (setUpProps? : any , container = shallow) => {
-  const props = {
-    addBlog: jest.fn(),
-    deleteBlog: jest.fn(),
-    blogs: blogListsForListing,
-  }
-  const wrapper  = container(<BlogsListing {...props} />);
-  return { wrapper, props};
-}
+import { blogListsForListing } from '../../../tests_helpers/blogs';
+const setUp = (setUpProps?: any, container = shallow) => {
+	const props = {
+		addBlog: jest.fn(),
+		deleteBlog: jest.fn(),
+		blogs: blogListsForListing,
+	};
+	const wrapper = container(<BlogsListing {...props} />);
+	return { wrapper, props };
+};
 it('renders without crashing', () => {
-  const div = setUp();
-  expect(div).toMatchSnapshot()
+	const div = setUp();
+	expect(div).toMatchSnapshot();
 });
--- src/components/ViewBlog/ViewBlog.test.tsx
+++ src/components/ViewBlog/ViewBlog.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme'
+import { shallow } from 'enzyme';
 import ViewBlog from './ViewBlog';
 
 it('renders without crashing', () => {
-  const div = shallow(<ViewBlog />)
-  expect(div).toMatchSnapshot()
+	const div = shallow(<ViewBlog />);
+	expect(div).toMatchSnapshot();
 });
