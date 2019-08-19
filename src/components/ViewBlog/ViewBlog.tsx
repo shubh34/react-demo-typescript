52ae6251386401b8f2df72801aa7fea6e836d630
Shubham Jain <shubhamjain.qwerty@gmail.com>
2019-08-19 19:41:52 +0100

    formating

--- src/components/ViewBlog/ViewBlog.tsx
+++ src/components/ViewBlog/ViewBlog.tsx
@@ -6,29 +6,31 @@ import { connect } from 'react-redux';
 import SectionHeader from '../sharedComponents/SectionHeader/SectionHeader';
 import ContentContainer from '../sharedComponents/ContentContainer/ContentContainer';
 import CategoriesContainer from '../sharedComponents/CategoriesContainer/CategoriesContainer';
-const mapState = (state : IRootState, props: any) => ({
+const mapState = (state: IRootState, props: any) => ({
 	hasBlog: hasBlogId(state, props.match.params.id),
-	blogDetails: getBlogById(state, props.match.params.id)
+	blogDetails: getBlogById(state, props.match.params.id),
 });
 type ReduxType = ReturnType<typeof mapState>;
 
-export const ViewBlog: React.FC<ReduxType> = (props) => {
-	const {blogDetails, hasBlog} = props;
-	
+export const ViewBlog: React.FC<ReduxType> = props => {
+	const { blogDetails, hasBlog } = props;
+
 	const renderNoBlogFound = () => {
-		return <div> No Blog found</div>
-	}
-	
-	if(!hasBlog) {
+		return <div> No Blog found</div>;
+	};
+
+	if (!hasBlog) {
 		return renderNoBlogFound();
 	}
 
-	const {title, content, categories } = blogDetails!
-	return <div>
-		<SectionHeader header={title}> </SectionHeader>
-		<ContentContainer content={content} readMore={false} />
-		<CategoriesContainer categories={categories} hasTagIcon/>
-	</div>;
+	const { title, content, categories } = blogDetails!;
+	return (
+		<div>
+			<SectionHeader header={title}> </SectionHeader>
+			<ContentContainer content={content} readMore={false} />
+			<CategoriesContainer categories={categories} hasTagIcon />
+		</div>
+	);
 };
 
-export default connect(mapState) (ViewBlog);
+export default connect(mapState)(ViewBlog);
