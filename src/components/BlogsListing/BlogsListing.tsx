a9c94312d9fa637d00e7cdf8343d4d1b6b159414
Shubham Jain <shubhamjain.qwerty@gmail.com>
2019-08-20 01:23:40 +0100

    No blog

--- src/components/BlogsListing/BlogsListing.tsx
+++ src/components/BlogsListing/BlogsListing.tsx
@@ -7,6 +7,7 @@ import { Dispatch } from 'redux';
 import { getBlogList } from '../../states/blogs/selectors';
 import Blog from '../../sharedComponents/Blog/Blog';
 import SectionHeader from '../../sharedComponents/SectionHeader/SectionHeader';
+import NoBlogsComponent from '../../sharedComponents/NoBlogsComponent/NoBlogsComponent';
 
 const mapState = (state: IRootState) => ({
 	blogs: getBlogList(state),
@@ -21,6 +22,7 @@ export const BlogsListing: React.FC<ReduxType> = ({ blogs, deleteBlog }) => {
 	return (
 		<div>
 			<SectionHeader header='Blogs' />
+			{blogs.length === 0 && <NoBlogsComponent />}
 			{blogs.map((blog: any) => (
 				<Blog key={blog.id} blog={blog} deleteBlog={deleteBlog} />
 			))}
---
+++ src/sharedComponents/NoBlogsComponent/NoBlogsComponent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SectionHeader from '../SectionHeader/SectionHeader';
+
+const NoBlogsComponent: React.FC = () => {
+	return <SectionHeader header={"Seems like you haven't created any blog.Try adding new blog"} />;
+};
+
+export default NoBlogsComponent;
